8edfa9eee743d15e951236bd0d3691bfefc8c610
vikasmourya <60725905+thisisvikasmourya@users.noreply.github.com>
2023-01-23 14:09:05 +0530

    done-22

--- pages/thankyou.js
+++ pages/thankyou.js
@@ -9,9 +9,9 @@ const ThankYou = () => {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <div className="pt-40 px-5 bg-darkBg text-primary text-center text-3xl ">
-      <h1>Thank you for contacting Us </h1>
-      <h2>OUr Team will reply soon</h2>
+      <div className=" pt-25 lg:pt-60 px-5 pb-40 bg-darkBg text-primary text-center text-3xl ">
+      <h1>Thank you🙂 for contacting Us </h1>
+      <h2>Our Team will reply soon!!!</h2>
       </div>
     </div>
   );

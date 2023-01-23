0c99d76e5903342e0e9578cf4c43181bd56ee4c5
vikasmourya <60725905+thisisvikasmourya@users.noreply.github.com>
2023-01-23 13:56:09 +0530

    done-final3

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,17 +1,17 @@
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
-const email = process.env.EMAIL
-const pass = process.env.EMAIL_PASS
+const email = process.env.EMAIL;
+const pass = process.env.EMAIL_PASS;
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-      user: email,
-      pass,
+    user: email,
+    pass,
   },
-} );
+});
 
-
+const emailID = "info@fintlivest.com";
 export const mailOptions = {
-    from: email,
-    to: email,
-}
+  from: email,
+  to: emailID,
+};
